0c2c91cdd1d73d1b85dd3f3d63dd9543a0e1477d
Jonathan Guiang <jkguiang@gmail.com>
2019-03-06 17:30:54 -0800

    moved front side files to 'front' dir

---
+++ src/front/FrontNav.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Nav, NavItem, NavLink } from 'reactstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Bubble } from '../Animations.js';
+
+class FrontNav extends Component {
+  render() {
+     return (
+       <div className="sticky-top" style={{ height: "0%", paddingRight: "5%" }}>
+         <Nav className="justify-content-end">
+           <NavItem>
+             <LinkContainer to="/">
+               <Bubble><NavLink disabled={this.props.target === 0} href="/">Home</NavLink></Bubble>
+             </LinkContainer>
+           </NavItem>
+           <NavItem>
+             <LinkContainer to="/about">
+               <Bubble><NavLink disabled={this.props.target === 1} href="/about">About</NavLink></Bubble>
+             </LinkContainer>
+           </NavItem>
+           <NavItem>
+             <LinkContainer to="/signin">
+               <Bubble><NavLink disabled={this.props.target === 2} href="/signin">Sign In <FontAwesomeIcon icon="sign-in-alt"/></NavLink></Bubble>
+             </LinkContainer>
+           </NavItem>
+         </Nav>
+       </div>
+     );
+   }
+ }
+
+export default FrontNav;
