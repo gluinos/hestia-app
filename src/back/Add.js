3f212cca9a5a77480a010639e694ed8f1a81a2ff
Jonathan Guiang <jkguiang@gmail.com>
2019-03-31 11:20:00 -0700

    added dummy form

--- src/back/Add.js
+++ src/back/Add.js
@@ -1,8 +1,61 @@
 import React, { Component, Fragment } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Container, Row, Nav, NavItem, NavLink } from 'reactstrap';
+import { Container, Row, Col, Nav, NavItem, NavLink,
+         Form, FormGroup, Label, Input } from 'reactstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Bubble, ContentContainer, SwingLeftContent } from '../Animations.js';
+import BackNav from './BackNav.js';
+
+class AddForm extends Component {
+  render() {
+    return (
+      <Form>
+        <SwingLeftContent><Row form>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="exampleEmail">Email</Label>
+              <Input type="email" name="email" id="exampleEmail" placeholder="user@example.com" />
+            </FormGroup>
+          </Col>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input type="password" name="password" id="examplePassword" placeholder="password" />
+            </FormGroup>
+          </Col>
+        </Row></SwingLeftContent>
+        <SwingLeftContent><FormGroup>
+          <Label for="exampleAddress">Address</Label>
+          <Input type="text" name="address" id="exampleAddress" placeholder="1234 Main St"/>
+        </FormGroup></SwingLeftContent>
+        <SwingLeftContent><FormGroup>
+          <Label for="exampleAddress2">Address 2</Label>
+          <Input type="text" name="address2" id="exampleAddress2" placeholder="Apartment, studio, or floor"/>
+        </FormGroup></SwingLeftContent>
+        <SwingLeftContent><Row form>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="exampleCity">City</Label>
+              <Input type="text" name="city" id="exampleCity"/>
+            </FormGroup>
+          </Col>
+          <Col md={4}>
+            <FormGroup>
+              <Label for="exampleState">State</Label>
+              <Input type="text" name="state" id="exampleState"/>
+            </FormGroup>
+          </Col>
+          <Col md={2}>
+            <FormGroup>
+              <Label for="exampleZip">Zip</Label>
+              <Input type="text" name="zip" id="exampleZip"/>
+            </FormGroup>
+          </Col>
+        </Row></SwingLeftContent>
+      </Form>
+    );
+  }
+}
 
 class Dashboard extends Component {
   render() {
@@ -11,6 +64,7 @@ class Dashboard extends Component {
          <ContentContainer>
            <Container fluid>
                <Row>
+                 <BackNav target={this.props.location.pathname}/>
                  <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
                    <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                      <SwingLeftContent><h2 className="display-5">Add Someone</h2></SwingLeftContent>
@@ -25,12 +79,12 @@ class Dashboard extends Component {
                      </div>
                    </div>
                    <div>
+                     <AddForm/>
 
                    </div>
                  </main>
                </Row>
-             </Container>
-
+             </Container>s
          </ContentContainer>
        </Fragment>
      );
