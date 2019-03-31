f502829e642bc476bd361cd32964dd52e4c2f776
Jonathan Guiang <jkguiang@gmail.com>
2019-03-31 11:19:53 -0700

    fixed formatting

--- src/back/BackNav.js
+++ src/back/BackNav.js
@@ -11,45 +11,45 @@ class BackNav extends Component {
            <div className="sidebar-sticky">
              <Nav className="flex-column">
                <NavItem>
-               <LinkContainer to="/dashboard">
-                 <Bubble><NavLink disabled={this.props.target === "/dashboard"} href="/dashboard">
-                   <FontAwesomeIcon icon="tachometer-alt" /> Dashboard
-                 </NavLink></Bubble>
-               </LinkContainer>
+                 <LinkContainer to="/dashboard">
+                   <Bubble><NavLink disabled={this.props.target === "/dashboard"} href="/dashboard">
+                     <FontAwesomeIcon icon="tachometer-alt" /> Dashboard
+                   </NavLink></Bubble>
+                 </LinkContainer>
                </NavItem>
                <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
                  <span>Database</span>
                </h6>
                <NavItem>
-               <LinkContainer to="/dashboard/add">
-                 <Bubble><NavLink disabled={this.props.target === "/dashboard/add"} href="/dashboard/add">
-                   <FontAwesomeIcon icon="user-plus" /> Add
-                 </NavLink></Bubble>
-               </LinkContainer>
+                 <LinkContainer to="/dashboard/add">
+                   <Bubble><NavLink disabled={this.props.target === "/dashboard/add"} href="/dashboard/add">
+                     <FontAwesomeIcon icon="user-plus" /> Add
+                   </NavLink></Bubble>
+                 </LinkContainer>
                </NavItem>
                <NavItem>
-               <LinkContainer to="/dashboard/people">
-                 <Bubble><NavLink disabled={this.props.target === "/dashboard/people"} href="/dashboard/people">
-                   <FontAwesomeIcon icon="users" /> People
-                 </NavLink></Bubble>
-               </LinkContainer>
+                 <LinkContainer to="/dashboard/people">
+                   <Bubble><NavLink disabled={this.props.target === "/dashboard/people"} href="/dashboard/people">
+                     <FontAwesomeIcon icon="users" /> People
+                   </NavLink></Bubble>
+                 </LinkContainer>
                </NavItem>
                <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
                  <span>Organization</span>
                </h6>
                <NavItem>
-               <LinkContainer to="/dashboard/settings">
-                 <Bubble><NavLink disabled={this.props.target === "/dashboard/settings"} href="/dashboard/settings">
-                   <FontAwesomeIcon icon="cog" /> Settings
-                 </NavLink></Bubble>
-               </LinkContainer>
+                 <LinkContainer to="/dashboard/settings">
+                   <Bubble><NavLink disabled={this.props.target === "/dashboard/settings"} href="/dashboard/settings">
+                     <FontAwesomeIcon icon="cog" /> Settings
+                   </NavLink></Bubble>
+                 </LinkContainer>
                </NavItem>
                <NavItem>
-               <LinkContainer to="/dashboard/request">
-                 <Bubble><NavLink disabled={this.props.target === "/dashboard/request"} href="/dashboard/request">
-                   <FontAwesomeIcon icon="microchip" /> Request
-                 </NavLink></Bubble>
-               </LinkContainer>
+                 <LinkContainer to="/dashboard/request">
+                   <Bubble><NavLink disabled={this.props.target === "/dashboard/request"} href="/dashboard/request">
+                     <FontAwesomeIcon icon="microchip" /> Request
+                   </NavLink></Bubble>
+                 </LinkContainer>
                </NavItem>
              </Nav>
            </div>
