291cdabf19c24ad49f997bfc08c70e67daf63cff
Jonathan Guiang <jkguiang@gmail.com>
2019-03-31 11:17:51 -0700

    added new charts

--- src/back/Charts.js
+++ src/back/Charts.js
@@ -1,19 +1,73 @@
 import React, { PureComponent } from 'react';
-import { ResponsiveContainer, PieChart, Pie } from 'recharts';
+import { ResponsiveContainer,
+         PieChart, Pie,
+         LineChart, Line,
+         BarChart, Bar,
+         CartesianGrid, Tooltip,
+         XAxis, YAxis,
+         Legend, } from 'recharts';
 
-const data = [
+const pieData = [
   { name: 'Group A', value: 400 }, { name: 'Group B', value: 300 },
   { name: 'Group C', value: 300 }, { name: 'Group D', value: 200 },
 ];
 
-class StaticPie extends PureComponent {
+const lineData = [
+  {
+    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+  },
+  {
+    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+  },
+  {
+    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+  },
+  {
+    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+  },
+  {
+    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+  },
+  {
+    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+  },
+  {
+    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+  },
+];
+
+const barData = [
+  {
+    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+  },
+  {
+    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+  },
+  {
+    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+  },
+  {
+    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+  },
+  {
+    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+  },
+  {
+    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+  },
+  {
+    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+  },
+];
+
+class PiePlot extends PureComponent {
 
   render() {
     return (
       <div style={{ width: '100%', height: 300 }}>
         <ResponsiveContainer>
           <PieChart>
-            <Pie dataKey="value" data={data} fill="#8884d8" innerRadius={60} outerRadius={70} label />
+            <Pie dataKey="value" data={pieData} fill="#8884d8" innerRadius={60} outerRadius={70} label />
           </PieChart>
         </ResponsiveContainer>
       </div>
@@ -21,4 +75,50 @@ class StaticPie extends PureComponent {
   }
 }
 
-export { StaticPie };
+class LinePlot extends PureComponent {
+
+  render() {
+    return (
+      <div style={{ width: '100%', height: 300 }}>
+        <ResponsiveContainer>
+          <LineChart
+            data={lineData}
+            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    );
+  }
+}
+
+class BarPlot extends PureComponent {
+
+    render() {
+      return (
+        <div style={{ width: '100%', height: 300 }}>
+          <ResponsiveContainer>
+            <BarChart
+              data={barData}
+              margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="pv" fill="#8884d8" />
+              <Bar dataKey="uv" fill="#82ca9d" />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      );
+    }
+}
+
+export { PiePlot, LinePlot, BarPlot };
