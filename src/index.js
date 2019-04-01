ef84c354e5c0823413e4291a56645abbc34b1c46
Jonathan Guiang <jkguiang@gmail.com>
2019-03-31 20:18:58 -0700

    added/modified quill css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-quill/dist/quill.snow.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
